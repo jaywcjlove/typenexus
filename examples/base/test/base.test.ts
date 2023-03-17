6da37d8862303dc428b06eca321bce79b8b6fd92
jaywcjlove <398188662@qq.com>
2023-03-17 17:12:01 +0800

    test: update test case.

--- examples/base/test/base.test.ts
+++ examples/base/test/base.test.ts
@@ -108,21 +108,20 @@ const options: TypeNexusOptions = {
     .expect(200)
   assert.deepEqual(req.body, { });
 
+  ;(async () => {
+    const app = new TypeNexus(3001, options);
+    await app.connect();
+
+    app.controllers([UserController]);
+    const testSession = await session(app.app);
+
+    console.log('\x1b[32;1m POST\x1b[0m /users/session \x1b[34;1m @Session/@SessionParam\x1b[0m');
+    let req = await testSession
+      .post('/users/session')
+      .send({ username: 'foo', password: 'password' })
+      .expect('Content-Type', /json/)
+      .expect(200)
+    assert.deepEqual(req.body, { id: 12, session: ['cookie'], cookie: ['path', '_expires', 'originalMaxAge', 'httpOnly'] });
+  })();
+
 })();
-
-;(async () => {
-
-  const app = new TypeNexus(3001, options);
-  await app.connect();
-
-  app.controllers([UserController]);
-  const testSession = await session(app.app);
-
-  console.log('\x1b[32;1m POST\x1b[0m /users/session \x1b[34;1m @Session/@SessionParam\x1b[0m');
-  let req = await testSession
-    .post('/users/session')
-    .send({ username: 'foo', password: 'password' })
-    .expect('Content-Type', /json/)
-    .expect(200)
-  assert.deepEqual(req.body, { id: 12, session: ['cookie'], cookie: ['path', '_expires', 'originalMaxAge', 'httpOnly'] });
-})()
